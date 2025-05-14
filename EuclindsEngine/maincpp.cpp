a6faa40d92b0184f2b9cbe2f7704be541cd1561a
Abhxorus <artemiswolfer@gmail.com>
2025-05-14 11:27:55 -0600

    setup render pipeline

--- EuclindsEngine/maincpp.cpp
+++ EuclindsEngine/maincpp.cpp
@@ -1,31 +1,64 @@
 #include <SFML/Graphics.hpp>
 
-int main()
+sf::RenderWindow* window;
+sf::CircleShape* circle;//creamos el puntero
+
+void
+Init()
 {
-    // Crear la ventana
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Ejemplo simple de SFML");
+    window = new sf::RenderWindow(sf::VideoMode(1000, 800), "Euclinds Engine");
+    circle = new sf::CircleShape(100.0f);//inicializamos el puntero
+    circle->setFillColor(sf::Color::Magenta);//le damos informacion al puntero
+    circle->setPosition(200.f, 150.f);
+}
 
-    // Crear un círculo
-    sf::CircleShape circle(100.f); // radio 100
-    circle.setFillColor(sf::Color::Magenta);
-    circle.setPosition(200.f, 150.f);
-
-    // Bucle principal
-    while (window.isOpen())
+void
+HandleEvents()
+{
+    sf::Event event;
+    while (window->pollEvent(event))
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        //cerrar la ventana si el usuario lo indica
+        if (event.type == sf::Event::Closed)
         {
-            // Cerrar la ventana si el usuario lo indica
-            if (event.type == sf::Event::Closed)
-                window.close();
+            window->close();
         }
-
-        // Limpiar, dibujar y mostrar
-        window.clear();
-        window.draw(circle);
-        window.display();
     }
 
+}
+
+void
+Update()
+{
+    //Aqui va la logica
+}
+
+void
+Render()
+{
+    window->clear();
+    window->draw(*circle);//dibujamos el circulo
+    window->display();
+}
+
+void
+Destroy()
+{
+    delete window;
+    delete circle;//destruimos el circulo
+}
+
+int main()
+{
+    Init();
+
+    while (window->isOpen())
+    {
+        HandleEvents();
+        Update();
+        Render();
+    }
+
+    Destroy();
     return 0;
 }
